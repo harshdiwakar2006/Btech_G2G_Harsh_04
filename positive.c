19e9c18c85d9bd086872b8e00dc8aa0b7d0b96ae
harshdiwakar2006 <harshdiwakar2006@gmail.com>
2026-02-16 10:32:26 +0530

    Add files via upload

---
+++ positive.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int a;
+    printf("enter a number : ");
+    scanf("%d",&a);
+    if (a > 0){
+        printf("%d is a positive number ",a);
+    }
+    else if (a < 0){
+        printf("%d is a negative number ",a);
+    }
+    else {
+        printf("%d is zero",a);
+    }
+    return 0;
+
+}
